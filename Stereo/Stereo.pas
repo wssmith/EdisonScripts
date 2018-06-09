fcb055b544fb4d51f72a3d56208bf02e19467f32
Wesley Smith <WesleySmith163@gmail.com>
2018-06-09 14:42:53 -0700

    Update Stereo.pas

--- Stereo/Stereo.pas
+++ Stereo/Stereo.pas
@@ -22,21 +22,20 @@ procedure MonoToStereo();
 var n, x1, x2 : Integer; 
     s : Single;
 begin
+    EditorSample.NumChans := 2;
 
-EditorSample.NumChans := 2;
+    // Copy the entire sample
+    x1 := 0;
+    x2 := EditorSample.Length - 1;
 
-// Copy the entire sample
-x1 := 0;
-x2 := EditorSample.Length - 1;
+    for n := x1 to x2 do 
+    begin
+        // Progress message
+        if ((n - x1) mod 10000) = 0 then ProgressMsg('Stereoizing sample...', n - x1, x2 - x1);
 
-for n := x1 to x2 do 
-begin
-    // Progress message
-    if ((n - x1) mod 10000) = 0 then ProgressMsg('Stereoizing sample...', n - x1, x2 - x1);
-        
-    s := EditorSample.GetSampleAt(n, 0);
-    EditorSample.SetSampleAt(n, 1, s); // right
-end; // n
+        s := EditorSample.GetSampleAt(n, 0);
+        EditorSample.SetSampleAt(n, 1, s); // right
+    end; // n
 end; // end function MonoToStereo
 
 end.
